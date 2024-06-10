0d3d1ff08f335e06a24f7be2e483c55e5332447d
Piyush-Sharma79 <g2001idseptember@gmail.com>
2024-06-10 17:51:53 +0530

    hmm

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,18 +2,63 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
+const Post = require('../models/post');
+
+//get-home
+
+
+router.get('/', async(req,res)=>{
     const locals = {
         title: 'Home',
         description: 'This is the home page'
     }
+    try{
+        const data = await Post.find();
+        res.render('index',{locals,data});
+    }catch(error){
+        console.log(error);
+    }
 
-
-    res.render('index',{locals});
+    
 });
-router.get('/about',(req,res)=>{
-    res.render('about');
+
+//get-about
+
+router.get('/about',async (req,res)=>{
+    const locals = {
+        title: 'About',
+        description: 'This is the about page'
+    }
+    res.render('about',{locals});    
 });
 
+//get-contact
+
+router.get('/contact',(req,res)=>{
+    const locals = {
+        title: 'Contact',
+        description: 'This is the contact page'
+    }
+    res.render('contact',{locals});
+});
+
+
+// function insertPostData(){
+//     Post.insertMany([
+//         {
+//             title: 'Post 1',
+//             body: 'This is the body of post 1'
+//         },
+//         {
+//             title: 'Post 2',
+//             body: 'This is the body of post 2'
+//         },
+//         {
+//             title: 'Post 3',
+//             body: 'This is the body of post 3'
+//         }
+//     ])
+// }
+// insertPostData();
 
 module.exports = router;
