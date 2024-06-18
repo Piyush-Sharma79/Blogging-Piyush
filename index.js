30f3074c63e14ce98f084307c65f02a972b03c47
Piyush-Sharma79 <g2001idseptember@gmail.com>
2024-06-18 18:07:06 +0530

    .

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const expressLayout = require('express-ejs-layouts');
 const cookieParser = require('cookie-parser');
 const MongoStore = require('connect-mongo');
 const sessions = require('express-session');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const cors = require('cors');
 
 const {isActiveRoute} = require('./server/helpers/routeHelper');
 
@@ -20,6 +21,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
+app.use(cors());
 
 app.use(sessions({
     secret: 'keyboard cat',
